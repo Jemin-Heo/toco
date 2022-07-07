8b2e578b2e8541defb6608a920e267ce3fe1250b
Jemin-Heo <101089749+Jemin-Heo@users.noreply.github.com>
2022-07-07 16:49:20 +0900

    필요없는 스크립트파일 제거

--- java1.js
+++
@@ -1,6 +0,0 @@
-// new WOW().init();
-// src="//dapi.kakao.com/v2/maps/sdk.js?appkey=c878f6eac0a2c4a708e717a953b9ac76"
-// src="http://code.jquery.com/jquery-latest.js"
-// src="./wow.min.js"
-// src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"
-// src="https://kit.fontawesome.com/af2a9fd141.js" crossorigin="anonymous"
